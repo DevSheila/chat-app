7fcf1e0f6ec69bccb6c4719f364193850bd3a7da
DevSheila <devsheilawambui@gmail.com>
2024-09-30 14:46:57 +0300

    clean up code

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -2,8 +2,6 @@ import { createRouteHandler } from "uploadthing/next";
 
 import { ourFileRouter } from "./core";
 
-const secret = process.env.UPLOADTHING_SECRET;
-console.log("UPLOADTHING_SECRET:", secret); // Add this line
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
